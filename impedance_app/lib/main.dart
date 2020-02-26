b7138da562f8031559b68264b67e398df28625bb
rtb39 <rtb39@zips.uakron.edu>
2020-02-26 18:55:01 -0500

    Work on sign in page layout

--- impedance_app/lib/main.dart
+++ impedance_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,42 +14,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'BioImpedance Tracker',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
+      title: 'BioImpedance Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('BioImpedance Tracker'),
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text('BioImpedance Tracker'),
-          ),
-          body: Card(
-            child: Column(
-              children: <Widget>[
-                Text('Welcome: Click the button to view info'),
-                Image.asset('assets/HomePage.jpg'),
-                Container(
-                  margin: EdgeInsets.all(50),
-                  child: RaisedButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => PageOne()),
-                      );
-                    },
-                    child: Text('Sign In'),
-                    )
-                  ),
-              ]
+        body: Column(children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.all(20.0),
+            child: Text('Bioimpedance Tracker App',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 25.0,
+              ),
             ),
           ),
-        )
+          Container(
+            child: Image.asset('assets/body.jpg'),
+          ),
+          Container(
+              margin: EdgeInsets.all(50),
+              child: RaisedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => PageOne()),
+                  );
+                },
+                child: Text('Sign In',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                ),
+              )
+          ),
+        ]
+        ),
+      ),
     );
   }
 }
 
-class PageOne extends StatelessWidget{
+class PageOne extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Impedance Calculations'),
